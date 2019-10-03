fb5a57d4f02dfa1a1536c245014c59248ab38216
charliecao02 <55104201+charliecao02@users.noreply.github.com>
2019-10-03 12:29:18 -0700

    Add files via upload

--- Assignment-2.4/Exercise5.java
+++ Assignment-2.4/Exercise5.java
@@ -3,7 +3,15 @@ class Exercise5{
     public static void main(){
         Scanner scan=new Scanner(System.in);
         System.out.println("What character do you want your triangle made of?");
-        String 
-
+        String character=scan.next();
+        System.out.println("How tall do you want your triangle?");
+        int height=scan.nextInt();
+        System.out.println("Here's your triangle!");
+        for(int i=1;i<=height;i++){
+            for(int i2=1;i2<=i;i2++){
+                System.out.print(character);
+            }
+            System.out.print("\n");
+        }
     }
 }
